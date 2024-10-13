b1e90019d8422fd838278b14007e424e030b0b53
baguchan <shutux500@gmail.com>
2024-10-13 10:16:39 +0900

    oops

--- src/main/java/baguchan/taunt/network/TauntSyncPacket.java
+++ src/main/java/baguchan/taunt/network/TauntSyncPacket.java
@@ -1,34 +1,22 @@
 package baguchan.taunt.network;
 
-import bagu_chan.bagus_lib.util.client.AnimationUtil;
-import baguchan.taunt.Taunt;
 import baguchan.taunt.TauntMod;
-import baguchan.taunt.attachment.TauntAttachments;
 import baguchan.taunt.registry.ModAttachments;
-import baguchan.taunt.registry.Taunts;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.Holder;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 import net.neoforged.neoforge.network.handling.IPayloadHandler;
-
-import java.util.Optional;
 
 public class TauntSyncPacket implements CustomPacketPayload, IPayloadHandler<TauntSyncPacket> {
 
     public static final StreamCodec<FriendlyByteBuf, TauntSyncPacket> STREAM_CODEC = CustomPacketPayload.codec(
             TauntSyncPacket::write, TauntSyncPacket::new
     );
-    public static final Type<TauntSyncPacket> TYPE = new Type<>(ResourceLocation.fromNamespaceAndPath(TauntMod.MODID, "taunt"));
+    public static final Type<TauntSyncPacket> TYPE = new Type<>(ResourceLocation.fromNamespaceAndPath(TauntMod.MODID, "taunt_sync"));
 
     public int entityID;
     public ResourceLocation taunt;
