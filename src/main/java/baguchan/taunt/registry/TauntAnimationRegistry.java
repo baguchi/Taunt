812fa7ac89600bdbc17472a329be888189fd9517
baguchan <shutux500@gmail.com>
2024-10-13 10:16:15 +0900

    finally...

--- src/main/java/baguchan/taunt/registry/TauntAnimationRegistry.java
+++ src/main/java/baguchan/taunt/registry/TauntAnimationRegistry.java
@@ -3,10 +3,12 @@ package baguchan.taunt.registry;
 import bagu_chan.bagus_lib.event.RegisterBagusAnimationEvents;
 import baguchan.taunt.TauntMod;
 import net.minecraft.world.entity.player.Player;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 
 @EventBusSubscriber(modid = TauntMod.MODID)
 public class TauntAnimationRegistry {
+    @SubscribeEvent
     public static void registryAnimaiton(RegisterBagusAnimationEvents events){
         if(events.getEntity() instanceof Player) {
             TauntMod.registryAccess().registryOrThrow(Taunts.TAUNT_VARIANT_REGISTRY_KEY).holders().forEach(taunt -> {
